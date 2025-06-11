2655a71296faeb522c118ddef15c5050255c0b47
syahrul <82451876+AnimeGanz@users.noreply.github.com>
2025-06-11 23:06:56 +0700

    skenario1.php

--- skenario1.php
+++ skenario1.php
@@ -20,35 +20,4 @@ else if($nilai <= 69 && $nilai > 0)
 else {
     echo "nilai wajib di atas 0";
 }
-
- 
-
-?>
-
-<?php
-$waktu=date("18.00");
-if($waktu >=date(00.00) && $waktu <=date(04.00))
-{
-    echo $waktu,"Dini hari";
-}
-else if($waktu >=date(04.00 ) && $waktu <=date(10.00))
-{
-    echo $waktu,"Pagi hari";
-}
-else if($waktu >=date(10.00 ) && $waktu <=date(15.00))
-{
-    echo $waktu,"Siang hari";
-}
-else if($waktu >=date(15.00 ) && $waktu <=date(18.00))
-{
-    echo $waktu,"Sore hari";
-}
-else if($waktu >=date(18.00 ) && $waktu <=date(00.00))
-{
-    echo $waktu,"Malam hari";
-}
-else
-{
-    echo "Anda di indonesia";
-}
 ?>
